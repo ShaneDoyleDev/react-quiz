9e656db5e349fa056417700bd6510b0635838b68
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-20 14:59:45 +0100

    add reducer state reducer to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useEffect } from "react";
+import { useReducer, useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import "./App.css";
 
+const initialState = {
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  questions: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, status: "ready", questions: action.payload };
+    case "dataNotRecieved":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch("http://localhost:8000/questions");
         const data = await response.json();
-        console.log(data);
+        dispatch({ type: "dataRecieved", payload: data });
       } catch (error) {
-        console.error("Error fetching data:", error);
+        dispatch({ type: "dataNotRecieved" });
       }
     }
     fetchData();
