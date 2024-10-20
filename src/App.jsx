611654076a15bf7deffae19f66b3fc9c65483673
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-20 14:15:10 +0100

    fetch data on mount for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import "./App.css";
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import "./App.css";
 
 export default function App() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:8000/questions");
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
