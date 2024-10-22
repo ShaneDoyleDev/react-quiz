d9ad77c4547aba6370c601f935a55a85028d2a03
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-22 14:00:48 +0100

    setup useReducer in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
+import { useReducer } from "react";
 import "../App.css";
 
+const initialState = {};
+
+function reducer(state, payload) {}
+
 export default function App() {
+  const [state, dispatch] = useReducer(initialState, reducer);
   return <div></div>;
 }
