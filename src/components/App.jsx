9ba58e740c49284acc045a04ad59d3597c240c61
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-22 14:28:27 +0100

    create Main component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,10 @@ export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question placeholder</p>
+      </Main>
     </div>
   );
 }
